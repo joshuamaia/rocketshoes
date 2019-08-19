d64a50ce6fa283416f014efea212f4f7abae7179
Joshua Maia Rodrigues <joshuamaia@codata.pb.gov.br>
2019-08-19 08:54:05 -0300

    Produto duplicado

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,13 +1,17 @@
+import produce from 'immer';
+
 export default function cart(state = [], action) {
   switch (action.type) {
     case 'ADD_TO_CART':
-      return [
-        ...state,
-        {
-          ...action.product,
-          amount: 1,
-        },
-      ];
+      return produce(state, draft => {
+        const productIdex = draft.findIndex(p => p.id === action.product.id);
+
+        if (productIdex >= 0) {
+          draft[productIdex].amount += 1;
+        } else {
+          draft.push({ ...action.product, amount: 1 });
+        }
+      });
     default:
       return state;
   }
