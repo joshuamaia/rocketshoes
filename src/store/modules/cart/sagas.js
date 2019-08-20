329d0647aa19b683d5a24ad4cf61842883865ad8
Joshua Maia Rodrigues <joshuamaia@codata.pb.gov.br>
2019-08-20 15:45:57 -0300

    React Toastify

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,7 +1,7 @@
 import { call, select, put, all, takeLatest } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 import { formatPrice } from '../../../util/format';
 import api from '../../../services/api';
-
 import { addToCartSucess, updateAmount } from './actions';
 
 function* addToCart({ id }) {
@@ -18,7 +18,7 @@ function* addToCart({ id }) {
   const amount = currentAmount + 1;
 
   if (amount > stockAmount) {
-    console.tron.warn('ERRO');
+    toast.error('Quantidade solicitada fora de estoque');
     return;
   }
 
